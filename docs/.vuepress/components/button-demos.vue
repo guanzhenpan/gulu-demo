6c6740096c4dd6e4a68a21db987f5d46d846a540
panguanzhen <panguanzhen@qq.com>
2020-04-08 20:57:26 +0800

    updata

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <g-button>默认按钮</g-button>
+
+    </div>
+
+</template>
+<script>
+    import  Button from '../../../src/button'
+
+    export default {
+        component: {
+        'g-button':Button
+        }
+    }
+</script>
